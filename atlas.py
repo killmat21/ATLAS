eba25e41b826c94febf13c3be064ab8e40c0a707
Killian Mathieu <killmat@protonmail.com>
2021-07-06 22:09:51 +0200

    signal handling to exit Atlas properly

--- atlas.py
+++ atlas.py
@@ -1,22 +1,25 @@
 import click
 from src.engine import Atlas
+from src.robot_killer import RobotKiller
 
 
 @click.command()
 @click.option("--test/--real-shit", default=True, help="Test or Live environment")
 def main(test: bool):
-    print(
-        """
+    print("""
           _        _________   _____             _          ______   
          / \      |  _   _  | |_   _|           / \       .' ____ \  
         / _ \     |_/ | | \_|   | |            / _ \      | (___ \_| 
        / ___ \        | |       | |   _       / ___ \      _.____`.  
      _/ /   \ \_  _  _| |_  _  _| |__/ | _  _/ /   \ \_  _| \____) | 
     |____| |____|(_)|_____|(_)|________|(_)|____| |____|(_)\______.' 
-    """
-    )
+    """)
+    robot_killer = RobotKiller()
     atlas = Atlas(is_test=test)
     print(atlas)
+    while not robot_killer.shutdown:
+        # Where startegy will go
+        pass
 
 
 if __name__ == "__main__":
